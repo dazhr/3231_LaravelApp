b81dd8a74a2918995b72e4ef980d2aeda0b06813
Dinda <dazhrabil@gmail.com>
2026-05-25 10:52:51 +0700

    -

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function index(){
-        return view('welcome');
+    function index(Request $request){
+
+     $query = Event::with('category')
+                      ->where('date', '>=', now())
+                      ->orderBy('date', 'asc');
+    if ($request->has('category') && $request->category != '') {
+        $query->whereHas('category', function ($q) use ($request) {
+        $q->where('slug', $request->category);
+        });
+    }
+
+     $events = $query->get();
+
+        return view('welcome', compact('events', 'categories'));
+        
     }
 }
